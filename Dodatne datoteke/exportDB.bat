611ae3c7c59a4b6cf860ceb6cee4511542768835
CrazyFreak <jakovbla4@gmail.com>
2022-11-08 13:14:25 +0100

    Temp

--- Dodatne datoteke/exportDB.bat
+++ Dodatne datoteke/exportDB.bat
@@ -1,7 +1,6 @@
-cd "C:\Program Files\PostgreSQL\14\bin"
+cd "C:/Program Files/PostgreSQL/14/bin" || psql -U postgres -d FERdvorane -c "SET client_encoding TO 'UTF8'; COPY dvorana TO 'C:/Users/Public/Documents/dvorane.csv' DELIMITER ',' CSV HEADER;"
+PAUSE
 
-psql -U postgres -d FERdvorane -c "SET client_encoding TO 'UTF8'; COPY dvorana TO 'D:/dvorane.csv' DELIMITER ',' CSV HEADER;"
-
-psql -U postgres -d FERdvorane -c "SET client_encoding TO 'UTF8'; SELECT json_agg(dvorana)::jsonb FROM dvorana;" -o "D:/dvorane_db.json"
-more +2 "D:/dvorane_db.json" > "D:/dvorane.json"
-del "D:/dvorane_db.json"
+psql -U postgres -d FERdvorane -c "SET client_encoding TO 'UTF8'; SELECT json_agg(dvorana)::jsonb FROM dvorana;" -o "C:/Users/Public/Documents/dvorane.json"
+more +2 "C:/Users/Public/Documents/dvorane_db.json" > "C:/Users/Public/Documents/dvorane.json"
+del "C:/Users/Public/Documents/dvorane_db.json"
