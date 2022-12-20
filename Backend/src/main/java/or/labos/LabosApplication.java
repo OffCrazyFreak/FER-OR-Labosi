9713f5530e02961c1ddd48d051a00a52172cf7f7
CrazyFreak <jakovbla4@gmail.com>
2022-12-20 09:58:03 +0100

    Renamed labos project to backend.

--- Backend/src/main/java/or/labos/LabosApplication.java
+++ Backend/src/main/java/or/labos/LabosApplication.java
@@ -3,9 +3,11 @@ package or.labos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * POKRENUTI NA DESKTOPU JER WINDOWS NE PODRZAVA DUGI PATH DATOTEKE
+ */
 @SpringBootApplication
 public class LabosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LabosApplication.class, args);
 	}
