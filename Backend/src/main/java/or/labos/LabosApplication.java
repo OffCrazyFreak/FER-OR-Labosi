884e4904dead5789bffb5dcc0d7141b91450c208
CrazyFreak <jakovbla4@gmail.com>
2022-12-20 10:14:45 +0100

    Added openapi.json in string format available on /api route

--- Backend/src/main/java/or/labos/LabosApplication.java
+++ Backend/src/main/java/or/labos/LabosApplication.java
@@ -3,11 +3,9 @@ package or.labos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * POKRENUTI NA DESKTOPU JER WINDOWS NE PODRZAVA DUGI PATH DATOTEKE
- */
 @SpringBootApplication
 public class LabosApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(LabosApplication.class, args);
 	}
